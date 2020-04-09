6f08edb213d7072682c7d408e48535e83801571b
JordanTreDaniel <lilj390150@live.com>
2020-04-08 23:18:04 -0500

    Modified getSongs selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,10 @@
 import { createSelector } from 'reselect';
 
 export const getUser = state => state.userInfo.user;
-export const getSongs = state => state.songs.songs;
+export const getSongs = state => {
+    const { songs } = state.songs;
+    return songs
+}
 export const getAccessToken = createSelector(
     getUser,
     user => user.accessToken
