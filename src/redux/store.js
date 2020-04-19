4948a3fa21591e2f1e098f395ef583a17e9ffdff
Nicholas Rode <nrode@MBPNR.hsd1.wa.comcast.net>
2020-04-18 22:15:04 -0700

    Implemneted Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,23 @@ import rootReducer from "./reducers";
 import createSagaMiddleware from "redux-saga";
 import sagas from "./sagas";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+  key: 'authType',
+  storage: storage,
+  whitelist: ["songs", "userInfo"] // which reducer want to store
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
 const sagaMiddleware = createSagaMiddleware();
 // const allMiddleWare = [sagaMiddleware];
 const store = createStore(
-  rootReducer,
+  pReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware))
-);
+  );
+const persistor = persistStore(store);
 
 sagas.forEach(saga => sagaMiddleware.run(saga));
 
-export default store;
+export { persistor, store };
