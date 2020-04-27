ea2ac41e9a2d8f90ff4e3153b17d29633093acb6
JordanTreDaniel <lilj390150@live.com>
2020-04-26 20:01:59 -0500

    Improve word cloud rendering

--- src/components/RapCloud.js
+++ src/components/RapCloud.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchSongDetails } from '../redux/actions';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Box } from '@material-ui/core';
+import { Typography, Box, Grid } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
 import * as selectors from '../redux/selectors';
 import { createCanvas } from 'canvas';
@@ -15,36 +15,31 @@ const useStyles = makeStyles({
 	},
 	lyrics: {
 		whiteSpace: 'pre-line'
-	}
+	},
+	wordCloudBox: {
+		// minWidth:
+	},
+	lyricBox: {}
 });
 
 const RapCloud = (props) => {
 	const classes = useStyles();
 	let { songId } = useParams();
 	const { fetchSongDetails, song } = props;
+	const { normalizedLyrics } = song;
 	useEffect(() => {
 		console.log('use EFfect!!', { fetchSongDetails, songId, song });
 		fetchSongDetails(songId);
 	}, []);
 
 	const renderCloud = () => {
-		const rawWords = song.lyrics && song.lyrics.split(' ');
-		const wordCount = rawWords.reduce(function(wordCount, word) {
-			const currentCount = wordCount[word] + 1 || 1;
-			wordCount[word] = currentCount;
-			return wordCount;
-		}, {});
-
-		const normalizedWords = Object.entries(wordCount).map(([ word, count ]) => {
-			return { text: word, size: 10 + count };
-		});
 		let layout = null;
 		layout = cloud()
 			.size([ 960, 500 ])
 			.canvas(function() {
 				return createCanvas(1, 1);
 			})
-			.words(normalizedWords)
+			.words(normalizedLyrics)
 			.padding(5)
 			.rotate(function() {
 				return ~~(Math.random() * 2) * 90;
@@ -56,7 +51,7 @@ const RapCloud = (props) => {
 			.on('end', draw)
 			.start();
 
-		function draw(normalizedWords) {
+		function draw(normalizedLyrics) {
 			const d3Selection = d3.select('svg');
 			const svg = d3Selection._groups[0][0];
 			if (svg) {
@@ -64,14 +59,14 @@ const RapCloud = (props) => {
 				return;
 			}
 			const result = d3
-				.select('body')
+				.select('#word-cloud-box')
 				.append('svg')
 				.attr('width', 850)
 				.attr('height', 350)
 				.append('g')
 				.attr('transform', 'translate(320, 200)')
 				.selectAll('text')
-				.data(normalizedWords)
+				.data(normalizedLyrics)
 				.enter()
 				.append('text')
 				.style('font-size', function(d) {
@@ -94,10 +89,24 @@ const RapCloud = (props) => {
 				{song.full_title}
 			</Typography>
 			<br />
-			<Typography variant="p" classes={{ root: classes.lyrics }}>
-				{/* {song.lyrics} */}
-				<Box>{song.lyrics ? renderCloud() : 'Waiting on the lambda...'}</Box>
-			</Typography>
+			<Grid container maxWidth={9} id="rap-cloud-content">
+				<Grid item sm={12} md={12} classes={{ root: classes.wordCloudBox }}>
+					<Box id="word-cloud-box">
+						{song.lyrics ? (
+							renderCloud()
+						) : (
+							<Typography variant="p" classes={{ root: classes.lyrics }}>
+								{`Preparing the Rap Cloud for ${song.full_title}`}
+							</Typography>
+						)}
+					</Box>
+				</Grid>
+				<Grid item sm={12} md={12} classes={{ root: classes.lyricBox }}>
+					<Typography variant="p" classes={{ root: classes.lyrics }}>
+						{song.lyrics}
+					</Typography>
+				</Grid>
+			</Grid>
 		</React.Fragment>
 	) : null;
 };
