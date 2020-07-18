9878e89b97e80721d6aa48e44a8dc3c83d669f64
JordanTreDaniel <lilj390150@live.com>
2020-07-17 23:52:16 -0500

    Implement loading in Search.js

--- src/connected/Search.js
+++ src/connected/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Input } from '@material-ui/core';
+import { Button, Input, Typography } from '@material-ui/core';
 import { searchSongs, setSongSearchTerm } from '../redux/actions';
 import * as selectors from '../redux/selectors';
 import SearchSongList from './SearchSongList';
@@ -31,7 +31,24 @@ class Search extends Component {
 				</div>
 
 				<div style={{ width: '80vw', margin: 'auto', textAlign: 'left' }}>
-					{loading ? <span style={{ margin: 'auto' }}>Loading...</span> : null}
+					{loading ? (
+						<div
+							style={{
+								width: '51vw',
+								height: '51vw',
+								margin: 'auto',
+								backgroundImage: 'url("https://media.giphy.com/media/1uLQUtPLbJMQ0/giphy.gif")',
+								backgroundRepeat: 'no-repeat',
+								backgroundSize: 'cover',
+								borderRadius: '50%',
+								display: 'flex'
+							}}
+						>
+							<div style={{ margin: 'auto', color: 'white', width: 'fit-content' }}>
+								<Typography variant="h3">Loading...</Typography>
+							</div>
+						</div>
+					) : null}
 					<SearchSongList songs={this.props.songs} />
 				</div>
 			</div>
