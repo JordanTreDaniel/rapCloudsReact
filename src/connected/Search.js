5d1f221f639597a3085a15cf6778adabf67e2e8d
JordanTreDaniel <lilj390150@live.com>
2020-07-18 00:05:51 -0500

    Make LoadingCloud Re-Usable

--- src/connected/Search.js
+++ src/connected/Search.js
@@ -1,60 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import LoadingCloud from '../components/LoadingCloud';
 import { Button, Input, Typography } from '@material-ui/core';
 import { searchSongs, setSongSearchTerm } from '../redux/actions';
 import * as selectors from '../redux/selectors';
 import SearchSongList from './SearchSongList';
+import { makeStyles } from '@material-ui/core/styles';
 
-class Search extends Component {
-	search = () => {
-		const { searchTerm } = this.props;
-		this.props.searchSongs(searchTerm);
+const useStyles = makeStyles({
+	outerLoading: {
+		width: '51vw',
+		height: '51vw',
+		margin: 'auto',
+		backgroundImage: 'url("https://media.giphy.com/media/1uLQUtPLbJMQ0/giphy.gif")',
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		borderRadius: '50%',
+		display: 'flex'
+	},
+	innerLoading: { margin: 'auto', color: 'white', width: 'fit-content' },
+	songListContainer: { width: '80vw', margin: 'auto', textAlign: 'left' }
+});
+
+const Search = (props) => {
+	const { setSongSearchTerm, searchTerm, loading } = props;
+	const search = () => {
+		const { searchTerm } = props;
+		props.searchSongs(searchTerm);
 	};
-
-	render = () => {
-		const { setSongSearchTerm, searchTerm, loading } = this.props;
-		return (
-			<div className={'masterBox'}>
-				<div className={'searchBar'}>
-					<Input
-						type="text"
-						onChange={(e) => {
-							const { value: searchTerm } = e.target;
-							setSongSearchTerm(searchTerm);
-						}}
-						value={searchTerm}
-						disableUnderline
-						fullWidth
-						placeholder="Search Songs..."
-					/>
-					<Button onClick={this.search}>Search</Button>
-				</div>
-
-				<div style={{ width: '80vw', margin: 'auto', textAlign: 'left' }}>
-					{loading ? (
-						<div
-							style={{
-								width: '51vw',
-								height: '51vw',
-								margin: 'auto',
-								backgroundImage: 'url("https://media.giphy.com/media/1uLQUtPLbJMQ0/giphy.gif")',
-								backgroundRepeat: 'no-repeat',
-								backgroundSize: 'cover',
-								borderRadius: '50%',
-								display: 'flex'
-							}}
-						>
-							<div style={{ margin: 'auto', color: 'white', width: 'fit-content' }}>
-								<Typography variant="h3">Loading...</Typography>
-							</div>
-						</div>
-					) : null}
-					<SearchSongList songs={this.props.songs} />
-				</div>
+	const classes = useStyles();
+	return (
+		<div className={'masterBox'}>
+			<div className={'searchBar'}>
+				<Input
+					type="text"
+					onChange={(e) => {
+						const { value: searchTerm } = e.target;
+						setSongSearchTerm(searchTerm);
+					}}
+					value={searchTerm}
+					disableUnderline
+					fullWidth
+					placeholder="Search Songs..."
+				/>
+				<Button onClick={search}>Search</Button>
 			</div>
-		);
-	};
-}
+
+			<div className={classes.songListContainer}>
+				{loading && <LoadingCloud />}
+				<SearchSongList songs={props.songs} />
+			</div>
+		</div>
+	);
+};
 
 const mapState = (state) => ({
 	songs: selectors.getSongsList(state),
