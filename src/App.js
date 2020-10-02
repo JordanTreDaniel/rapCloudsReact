ec023f6940c5a470fa5f6993a145e3006c816977
JordanTreDaniel <lilj390150@live.com>
2020-10-01 22:36:18 -0500

    Set up analytics 1.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/SignIn';
@@ -12,6 +12,17 @@ import * as selectors from './redux/selectors';
 import { connect } from 'react-redux';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import paths from './paths.js';
+import ReactGA from 'react-ga';
+import { history } from './redux/store';
+
+function initializeReactGA() {
+	ReactGA.initialize('UA-166594032-2');
+	ReactGA.pageview('/home');
+	history.listen((location) => {
+		ReactGA.set({ page: location.pathname });
+		ReactGA.pageview(location.pathname);
+	});
+}
 
 const theme = createMuiTheme({
 	background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
@@ -19,6 +30,9 @@ const theme = createMuiTheme({
 
 const App = (props) => {
 	const { user, appIsHydrated, location } = props;
+	useEffect(() => {
+		initializeReactGA();
+	}, []);
 	if (!user && appIsHydrated && location.pathname !== paths.signIn) {
 		console.log('APP rendered w/ no user after hydration, redirecting.');
 		return <Redirect to={paths.signIn} />;
