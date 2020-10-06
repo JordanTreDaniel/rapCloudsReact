045fe77cd4b680a58331635305f53d5656d19cb9
JordanTreDaniel <lilj390150@live.com>
2020-10-06 13:41:57 -0500

    New songList design

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,67 +1,59 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const styles = {
-	root: {
-		maxWidth: '',
-		minWidth: '20em',
-		margin: '2em'
-	},
-	media: {
-		height: 140
-	},
-	songList: {
-		display: 'flex',
-		justifyContent: 'space-evenly',
-		flexWrap: 'wrap'
-	}
-};
+const useStyles = makeStyles((theme) => {
+	return {
+		root: {
+			display: 'flex',
+			flexWrap: 'wrap',
+			justifyContent: 'space-around',
+			overflow: 'hidden',
+			backgroundColor: theme.palette.background.paper,
+			maxWidth: '',
+			minWidth: '20em',
+			margin: '2em'
+		},
+		media: {
+			height: 140
+		},
+		songList: {
+			display: 'flex',
+			justifyContent: 'space-evenly',
+			flexWrap: 'wrap'
+		},
+		gridList: {
+			width: 500,
+			height: 450
+		}
+	};
+});
 
 const SongList = (props) => {
-	const { classes, songs } = props;
+	const { songs } = props;
+	const classes = useStyles;
+	let cols = 1;
 	return (
-		<div className={classes.songList}>
-			{songs.map((song, idx) => {
-				return (
-					<Card className={classes.root} key={idx}>
-						<CardActionArea>
-							<CardMedia
-								className={classes.media}
-								image={song.header_image_thumbnail_url}
-								title={song.full_title}
-							/>
-							<CardContent>
-								<Typography gutterBottom variant="h5" component="h2">
-									{song.full_title}
-								</Typography>
-							</CardContent>
-						</CardActionArea>
-						<CardActions>
-							<Button
-								size="small"
-								color="primary"
-								href={`https://genius.com${song.path}`}
-								target="_blank"
-							>
-								Genius
-							</Button>
-							<Link to={`/clouds/${song.id}`}>
-								<Button size="small" color="primary">
-									RapCloud
-								</Button>
-							</Link>
-						</CardActions>
-					</Card>
-				);
-			})}
+		<div className={classes.root}>
+			<GridList cellHeight={160} className={classes.gridList} cols={3}>
+				{songs.map((song, idx) => {
+					const artist = song.primary_artist;
+					const { name: artistName = 'Unknown' } = artist || {};
+					const item = (
+						<GridListTile key={idx} cols={cols} component={Link} to={`/clouds/${song.id}`}>
+							<img src={song.header_image_thumbnail_url} alt={song.full_title} />
+							<GridListTileBar title={song.full_title} subtitle={<span>by: {artistName}</span>} />
+						</GridListTile>
+					);
+					if (cols === 3) {
+						cols = 1;
+					} else {
+						cols++;
+					}
+					return item;
+				})}
+			</GridList>
 		</div>
 	);
 };
@@ -70,5 +62,4 @@ SongList.defaultProps = {
 	songs: []
 };
 
-const StyledSongList = withStyles(styles)(SongList);
-export default StyledSongList;
+export default SongList;
