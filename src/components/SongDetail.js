ecda17f7d9054cf0d276796441959d6ee127664d
JordanTreDaniel <lilj390150@live.com>
2020-10-07 16:12:44 -0500

    Big songDetail redesign

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,6 +1,19 @@
 import React, { useEffect } from 'react';
 import { Redirect, useParams, Link } from 'react-router-dom';
-import { Typography, AppBar, Toolbar, Grid, Avatar, Tooltip } from '@material-ui/core';
+import {
+	Typography,
+	AppBar,
+	Toolbar,
+	Grid,
+	Avatar,
+	Tooltip,
+	LinearProgress,
+	Paper,
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { makeStyles } from '@material-ui/core/styles';
 import * as selectors from '../redux/selectors';
 import { fetchSongDetails } from '../redux/actions';
@@ -11,41 +24,64 @@ import BackButton from './BackButton';
 
 const useStyles = makeStyles((theme) => {
 	return {
+		songDetailContainer: {
+			height: '100%',
+			backgroundColor: theme.palette.primary.main,
+		},
 		buttonBox: {
-			display: 'flex'
+			display: 'flex',
 		},
 		toolBar: {
 			display: 'flex',
 			justifyContent: 'space-between',
-			backgroundColor: theme.palette.primary.light
-		},
-		homeLink: {
-			textDecoration: 'none',
-			color: 'black'
+			backgroundColor: theme.palette.primary.light,
 		},
 		artistBubbles: {
 			display: 'flex',
-			justifyContent: 'space-evenly'
+			justifyContent: 'space-evenly',
 		},
 		leftBubbles: {
 			display: 'flex',
-			justifyContent: 'space-evenly'
+			justifyContent: 'space-evenly',
 		},
 		lyrics: {
-			whiteSpace: 'pre-line'
+			whiteSpace: 'pre-line',
+			textAlign: 'center',
 		},
-		lyricBox: {},
+		mainContentChild: {},
 		loadingDiv: {
 			width: '100%',
 			'& > * + *': {
-				marginTop: theme.spacing(2)
+				marginTop: theme.spacing(2),
 			},
-			marginTop: '40%'
+			marginTop: '40%',
 		},
 		wordCloud: {
-			width: '90vw',
-			margin: 'auto'
-		}
+			width: '100%',
+			margin: 'auto',
+		},
+		songHeader: {
+			display: 'flex',
+			flexFlow: 'column wrap',
+			alignContent: 'center',
+		},
+		mainContent: {
+			overflowY: 'scroll',
+			width: '100%',
+			height: '100%',
+		},
+		header: {
+			color: theme.palette.text.secondary,
+			fontWeight: 600,
+		},
+		wordCloudPaper: {
+			padding: '1em',
+			margin: '1em',
+		},
+		lyricsPaper: {
+			padding: '1em',
+			margin: '1em',
+		},
 	};
 });
 
@@ -59,22 +95,22 @@ const SongDetail = (props) => {
 				fetchSongDetails(songId);
 			}
 		},
-		[ songId ]
+		[ songId ],
 	);
 	if (!songId) return <Redirect to={paths.search} />;
 	if (!song) return null;
 
 	const { full_title, path, writer_artists, primary_artist, lyrics, encodedCloud } = song;
 	const artists = writer_artists ? [ ...writer_artists ] : primary_artist ? [ primary_artist ] : [];
-
+	const [ briefedTitle, restOfTitle ] = full_title.split('by');
 	return (
-		<Grid>
+		<Grid className={classes.songDetailContainer}>
 			<AppBar color="inherit" position="static">
 				<Toolbar className={classes.toolBar}>
 					<div className={classes.leftBubbles}>
 						<BackButton />
-						<Tooltip placement="bottom" title={`See ${full_title} on Genius`}>
-							<a href={`https://genius.com${path}`} alt={`${song.full_title} on Genius`} target="_blank">
+						<Tooltip placement="bottom" title={`See ${briefedTitle} on Genius`}>
+							<a href={`https://genius.com${path}`} alt={`${briefedTitle} on Genius`} target="_blank">
 								<Avatar src="https://pbs.twimg.com/profile_images/885222003174551552/cv3KtGVS_400x400.jpg" />
 							</a>
 						</Tooltip>
@@ -93,25 +129,54 @@ const SongDetail = (props) => {
 					</div>
 				</Toolbar>
 			</AppBar>
-			<div>
-				<Typography variant="h3" className={classes.header}>
-					{full_title}
-				</Typography>
-			</div>
-			{isWordCloudLoading ? (
-				<LoadingCloud />
-			) : isSongDetailLoading || !lyrics ? null : (
-				<img src={`data:image/png;base64, ${encodedCloud}`} alt={'Rap Cloud'} className={classes.wordCloud} />
-			)}
-			{isSongDetailLoading ? (
-				<LoadingCloud />
-			) : (
-				<Grid item sm={12} md={12} classes={{ root: classes.lyricBox }}>
-					<Typography variant="body1" classes={{ root: classes.lyrics }}>
-						{lyrics}
-					</Typography>
+			<Grid className={classes.mainContent} container>
+				<Grid item xs={12}>
+					<div className={classes.songHeader}>
+						<Typography variant="h3" className={classes.header}>
+							{briefedTitle}
+						</Typography>
+						<Typography variant="small" className={classes.header}>
+							{`by ${restOfTitle}`}
+						</Typography>
+					</div>
 				</Grid>
-			)}
+				<Grid item sm={12} md={6} classes={{ root: classes.mainContentChild }}>
+					<Paper className={classes.wordCloudPaper}>
+						<Typography variant="h3" classes={{ root: classes.lyrics }}>
+							Cloud
+						</Typography>
+						{isWordCloudLoading ? (
+							<LoadingCloud />
+						) : isSongDetailLoading || !lyrics ? null : (
+							<img
+								src={`data:image/png;base64, ${encodedCloud}`}
+								alt={'Rap Cloud'}
+								className={classes.wordCloud}
+							/>
+						)}
+					</Paper>
+				</Grid>
+				<Grid item sm={12} md={6} classes={{ root: classes.mainContentChild }}>
+					<Paper className={classes.lyricsPaper}>
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls="panel1a-content"
+								id="panel1a-header"
+							>
+								<Typography variant="h3" classes={{ root: classes.lyrics }}>
+									Lyrics
+								</Typography>
+							</AccordionSummary>
+							<AccordionDetails>
+								<Typography variant="body1" classes={{ root: classes.lyrics }}>
+									{lyrics}
+								</Typography>
+							</AccordionDetails>
+						</Accordion>
+					</Paper>
+				</Grid>
+			</Grid>
 		</Grid>
 	);
 };
@@ -119,7 +184,7 @@ const SongDetail = (props) => {
 const mapState = (state) => ({
 	song: selectors.getCurrentSong(state),
 	isSongDetailLoading: selectors.isSongDetailLoading(state),
-	isWordCloudLoading: selectors.isWordCloudLoading(state)
+	isWordCloudLoading: selectors.isWordCloudLoading(state),
 });
 
 export default connect(mapState, { fetchSongDetails })(SongDetail);
