64688f420238dcf3a5c528909692c6d936f4a97f
JordanTreDaniel <lilj390150@live.com>
2020-10-09 00:53:46 -0500

    Dress up the SongDetail page

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -12,7 +12,9 @@ import {
 	Accordion,
 	AccordionSummary,
 	AccordionDetails,
+	IconButton,
 } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { makeStyles } from '@material-ui/core/styles';
 import * as selectors from '../redux/selectors';
@@ -21,12 +23,14 @@ import { connect } from 'react-redux';
 import paths from '../paths';
 import LoadingCloud from './LoadingCloud';
 import BackButton from './BackButton';
+import LoadingBar from './LoadingBar';
 
 const useStyles = makeStyles((theme) => {
 	return {
 		songDetailContainer: {
 			height: '100%',
 			backgroundColor: theme.palette.primary.main,
+			overflow: 'hidden',
 		},
 		buttonBox: {
 			display: 'flex',
@@ -48,7 +52,7 @@ const useStyles = makeStyles((theme) => {
 			whiteSpace: 'pre-line',
 			textAlign: 'center',
 		},
-		mainContentChild: {},
+		mainContentChild: { width: '100%' },
 		loadingDiv: {
 			width: '100%',
 			'& > * + *': {
@@ -64,14 +68,16 @@ const useStyles = makeStyles((theme) => {
 			display: 'flex',
 			flexFlow: 'column wrap',
 			alignContent: 'center',
+			textAlign: 'center',
 		},
 		mainContent: {
 			overflowY: 'scroll',
 			width: '100%',
 			height: '100%',
+			paddingBottom: '6em',
 		},
 		header: {
-			color: theme.palette.text.secondary,
+			color: theme.palette.secondary.light,
 			fontWeight: 600,
 		},
 		wordCloudPaper: {
@@ -82,6 +88,20 @@ const useStyles = makeStyles((theme) => {
 			padding: '1em',
 			margin: '1em',
 		},
+		sectionHeader: {
+			textAlign: 'center',
+			fontWeight: 600,
+			color: theme.palette.secondary.light,
+		},
+		headerAction: {
+			backgroundColor: theme.palette.secondary.light,
+			color: theme.palette.secondary.contrastText,
+			marginLeft: '1em',
+			'&:hover': {
+				backgroundColor: theme.palette.secondary.light,
+				color: theme.palette.secondary.contrastText,
+			},
+		},
 	};
 });
 
@@ -89,6 +109,8 @@ const SongDetail = (props) => {
 	const classes = useStyles();
 	const { song, fetchSongDetails, isSongDetailLoading, isWordCloudLoading } = props;
 	const { songId } = useParams();
+	const [ lyricsExpanded, setLyricsExpanded ] = React.useState(null);
+	const toggleLyricsExpanded = () => setLyricsExpanded(!lyricsExpanded);
 	useEffect(
 		() => {
 			if (songId) {
@@ -130,6 +152,7 @@ const SongDetail = (props) => {
 				</Toolbar>
 			</AppBar>
 			<Grid className={classes.mainContent} container>
+				<LoadingBar loading={isSongDetailLoading} />
 				<Grid item xs={12}>
 					<div className={classes.songHeader}>
 						<Typography variant="h3" className={classes.header}>
@@ -142,38 +165,37 @@ const SongDetail = (props) => {
 				</Grid>
 				<Grid item sm={12} md={6} classes={{ root: classes.mainContentChild }}>
 					<Paper className={classes.wordCloudPaper}>
-						<Typography variant="h3" classes={{ root: classes.lyrics }}>
+						<LoadingBar loading={isWordCloudLoading} />
+						<Typography variant="h3" classes={{ root: classes.sectionHeader }}>
 							Cloud
 						</Typography>
-						{isWordCloudLoading ? (
-							<LoadingCloud />
-						) : isSongDetailLoading || !lyrics ? null : (
-							<img
-								src={`data:image/png;base64, ${encodedCloud}`}
-								alt={'Rap Cloud'}
-								className={classes.wordCloud}
-							/>
-						)}
+						<img
+							src={
+								encodedCloud ? (
+									`data:image/png;base64, ${encodedCloud}`
+								) : (
+									`${process.env.PUBLIC_URL}/rapClouds.png`
+								)
+							}
+							alt={'Rap Cloud'}
+							className={classes.wordCloud}
+						/>
 					</Paper>
 				</Grid>
 				<Grid item sm={12} md={6} classes={{ root: classes.mainContentChild }}>
 					<Paper className={classes.lyricsPaper}>
-						<Accordion>
-							<AccordionSummary
-								expandIcon={<ExpandMoreIcon />}
-								aria-controls="panel1a-content"
-								id="panel1a-header"
-							>
-								<Typography variant="h3" classes={{ root: classes.lyrics }}>
-									Lyrics
-								</Typography>
-							</AccordionSummary>
-							<AccordionDetails>
-								<Typography variant="body1" classes={{ root: classes.lyrics }}>
-									{lyrics}
-								</Typography>
-							</AccordionDetails>
-						</Accordion>
+						<LoadingBar loading={false} /> {/* For spacing */}
+						<Typography variant="h3" classes={{ root: classes.sectionHeader }}>
+							Lyrics
+							<IconButton className={classes.headerAction} onClick={toggleLyricsExpanded}>
+								<AddIcon />
+							</IconButton>
+						</Typography>
+						{lyricsExpanded && (
+							<Typography variant="body1" classes={{ root: classes.lyrics }}>
+								{lyrics}
+							</Typography>
+						)}
 					</Paper>
 				</Grid>
 			</Grid>
