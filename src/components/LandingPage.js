d5a848560017d75b74b7a882f76b42d450a57f67
JordanTreDaniel <lilj390150@live.com>
2020-10-13 23:55:04 -0500

    Polish the look

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,19 +8,25 @@ const useStyles = makeStyles((theme) => {
 	return {
 		aboutPageContainer: {
 			minWidth: '100%',
-			minHeight: '100vh',
+			minHeight: '91vh',
 			maxWidth: '100vw',
 		},
 		fullSection: {
 			minWidth: '100%',
-			minHeight: '100%',
+			// minHeight: '100%',//Why doesn't 100% work?
+			minHeight: '91vh',
 			padding: '1em',
+			display: 'flex',
+			alignItems: 'center',
 		},
 		questionSection: {
 			backgroundColor: theme.palette.secondary.light,
 			color: theme.palette.primary.contrastText,
 		},
-		answerSection: {},
+		answerSection: {
+			marginTop: '2em',
+			marginBottom: '2em',
+		},
 		blueText: {
 			color: theme.palette.secondary.light,
 		},
@@ -39,20 +45,24 @@ const LandingPage = (props) => {
 	return (
 		<Paper classes={{ root: classes.aboutPageContainer }} elevation={0}>
 			<div className={classNames(classes.questionSection, classes.fullSection)}>
-				<Typography variant="h1">What are</Typography>
-				<Typography variant="h1">
-					<span className={classNames(classes.greyText, classes.bold)}>Rap Clouds</span>?
-				</Typography>
+				<div>
+					<Typography variant="h1">What are</Typography>
+					<Typography variant="h1">
+						<span className={classNames(classes.greyText, classes.bold)}>Rap Clouds</span>?
+					</Typography>
+				</div>
 			</div>
 			<div className={classNames(classes.fullSection, classes.answerSection)}>
-				<Typography variant="h3">
-					They're the <span className={classes.blueText}>lyrics</span> to your{' '}
-					<span className={classes.blueText}>favorite song</span>...
-				</Typography>
-				<br />
-				<Typography variant="h3">
-					...in a <span className={classes.blueText}>word cloud</span>.
-				</Typography>
+				<div>
+					<Typography variant="h3">
+						They're the <span className={classes.blueText}>lyrics</span> to your{' '}
+						<span className={classes.blueText}>favorite song</span>...
+					</Typography>
+					<br />
+					<Typography variant="h3">
+						...in a <span className={classes.blueText}>word cloud</span>.
+					</Typography>
+				</div>
 			</div>
 		</Paper>
 	);
