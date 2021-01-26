697bc296aebc49365c77a54c88de20980d4cebf2
JordanTreDaniel <lilj390150@live.com>
2021-01-25 22:38:13 -0600

    Indicate right/wrong answers

--- src/connected/ArtistGame.js
+++ src/connected/ArtistGame.js
@@ -30,7 +30,7 @@ import * as selectors from '../redux/selectors';
 import { searchSongs, setSongSearchTerm, fetchArtistGame, fetchSongDetails, answerQuestion } from '../redux/actions';
 import { connect } from 'react-redux';
 import paths from '../paths';
-import { classNames } from '../redux/utils';
+import { classNames } from '../utils';
 import LoadingBar from '../components/LoadingBar';
 import DebouncedInput from '../components/DebouncedInput';
 const DebouncedTextField = DebouncedInput(Input, { timeout: 639 });
@@ -49,17 +49,40 @@ const useStyles = makeStyles((theme) => {
 		artistName: {
 			fontSize: '2.4em',
 		},
+		miniCloud: {
+			marginRight: '.6em',
+		},
+		unanswered: {
+			color: theme.palette.primary.dark,
+		},
+		green: {
+			color: theme.palette.success.main,
+		},
+		red: {
+			color: theme.palette.error.main,
+		},
+		correctAnswer: {
+			border: `3px solid ${theme.palette.success.main} !important`,
+		},
+		incorrectAnswer: {
+			border: `3px solid ${theme.palette.error.main} !important`,
+		},
 		quizBoxContainer: {},
 		cloud: {
 			height: '90%',
 			margin: 'auto',
 		},
-		answerChoice: {
+		decoyAnswer: {
 			border: `3px solid ${theme.palette.secondary.main}`,
+		},
+		answerChoice: {
 			margin: '.6em',
 			borderRadius: '21px',
 			padding: 'inherit',
 			cursor: 'pointer',
+		},
+		scoreBoard: {
+			overflowX: 'scroll',
 		},
 	};
 });
@@ -68,8 +91,9 @@ const _QuizBox = (props) => {
 	const { question, fetchSongDetails, answerQuestion, song, clouds, gameId, questionIdx } = props;
 	const classes = useStyles();
 	const cloud = clouds[0] || {};
-	const { answers } = question;
+	const { answers, answerIdx } = question;
 	const { info } = cloud;
+	const isAnswered = answerIdx == 0 || answerIdx;
 	useEffect(() => {
 		if (!info) fetchSongDetails(song.id);
 	}, []);
@@ -80,16 +104,26 @@ const _QuizBox = (props) => {
 			</Grid>
 			<Grid item>
 				<List>
-					{answers.map((a, i) => (
-						<ListItem>
-							<Box
-								className={classes.answerChoice}
-								onClick={() => answerQuestion(gameId, questionIdx, i)}
-							>
-								<ListItemText>{a.title}</ListItemText>
-							</Box>
-						</ListItem>
-					))}
+					{answers.map((a, i) => {
+						const thisAnswerChosen = answerIdx == i;
+						return (
+							<ListItem>
+								<Box
+									className={classNames(
+										classes.answerChoice,
+										isAnswered
+											? a.correct
+												? classes.correctAnswer
+												: thisAnswerChosen ? classes.incorrectAnswer : classes.decoyAnswer
+											: classes.decoyAnswer,
+									)}
+									onClick={() => answerQuestion(gameId, questionIdx, i)}
+								>
+									<ListItemText>{a.title}</ListItemText>
+								</Box>
+							</ListItem>
+						);
+					})}
 				</List>
 			</Grid>
 		</Grid>
@@ -125,18 +159,18 @@ const ArtistGame = (props) => {
 				wrap="nowrap"
 				alignContent="center"
 				justify="space-around"
-				className={classes.ul}
+				className={classes.scoreBoard}
 			>
 				{questions.map((question, index) => {
 					let children;
 					const { answerIdx } = question;
 					const answer = question.answers[answerIdx];
 					if (!answer) {
-						children = <CloudQueue />;
+						children = <CloudQueue className={classNames(classes.miniCloud, classes.unanswered)} />;
 					} else if (answer.correct) {
-						children = <CloudDone />;
+						children = <CloudDone className={classNames(classes.miniCloud, classes.green)} />;
 					} else {
-						children = <CloudOff />;
+						children = <CloudOff className={classNames(classes.miniCloud, classes.red)} />;
 					}
 					return (
 						<Grid item key={index}>
