8f44070841b195547891529720fff86dadef112d
JordanTreDaniel <lilj390150@live.com>
2022-04-14 20:59:16 -0500

    Implement Finger Paint Font

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { Switch, Route } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { history } from './redux/store';
-import SplashScreen from './components/SplashScreen';
-import ErrorBoundary from './components/ErrorBoundary';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Paper } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { Switch, Route } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { history } from "./redux/store";
+import SplashScreen from "./components/SplashScreen";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Paper } from "@mui/material";
+// import createTypography from "material-ui/styles/typography";
+// import createPalette from "material-ui/styles/palette";
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
 	//NOTE: Why am I doing this with the store?
 	window.store = store;
-} 
+}
 
 const onBeforeLift = () => {
 	// take some action before the gate lifts
@@ -27,27 +29,30 @@ const onBeforeLift = () => {
  * Should I go for a scheme based of #9e9e9e & #0064ff?
  */
 const theme = createTheme({
-	background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+	typography: {
+		fontFamily: "Finger Paint",
+	},
+	background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
 	palette: {
 		primary: {
-			light: '#6d6d6d',
-			main: '#424242',
-			dark: '#1b1b1b',
-			contrastText: '#ffffff',
+			light: "#6d6d6d",
+			main: "#424242",
+			dark: "#1b1b1b",
+			contrastText: "#ffffff",
 		},
 		secondary: {
-			light: '#64c1ff',
-			main: '#0091ea',
-			dark: '#0064b7',
-			contrastText: '#f5f5f5',
+			light: "#64c1ff",
+			main: "#0091ea",
+			dark: "#0064b7",
+			contrastText: "#f5f5f5",
 		},
 		background: {
-			paper: '#6d6d6d',
-			default: '#424242',
+			paper: "#6d6d6d",
+			default: "#424242",
 		},
 		text: {
-			primary: '#ffffff',
-			secondary: '#0091ea',
+			primary: "#ffffff",
+			secondary: "#0091ea",
 		},
 	},
 	// shadows: defaultShadows.map((shadowString) => {
@@ -60,10 +65,10 @@ const theme = createTheme({
 	// 	});
 	// 	return result.join(',');
 	// }),
-	type: 'dark',
+	type: "dark",
 });
 history.listen((event, method) => {
-	const appContainer = document.getElementById('appContainer');
+	const appContainer = document.getElementById("appContainer");
 	if (appContainer) {
 		appContainer.scrollTop = 0;
 	}
@@ -72,8 +77,21 @@ ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<ThemeProvider theme={theme}>
-				<Paper id={'appContainer'} style={{ minHeight: '100vh', minWidth: '100vw' }} square elevation={0}>
-					<PersistGate loading={<SplashScreen />} onBeforeLift={onBeforeLift} persistor={persistor}>
+				<Paper
+					id={"appContainer"}
+					style={{ minHeight: "100vh", minWidth: "100vw" }}
+					square
+					elevation={0}
+				>
+					<style>
+						@import
+						url('https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap');
+					</style>
+					<PersistGate
+						loading={<SplashScreen />}
+						onBeforeLift={onBeforeLift}
+						persistor={persistor}
+					>
 						<ConnectedRouter history={history}>
 							<Switch>
 								<Route
@@ -91,7 +109,7 @@ ReactDOM.render(
 			</ThemeProvider>
 		</Provider>
 	</React.StrictMode>,
-	document.getElementById('root'),
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
